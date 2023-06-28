592442919a8460dd951d3c7e23f499ed9b0fb24e
RifaOrin <rifaorin1234@gmail.com>
2023-06-28 21:41:39 +0600

    Signup and login page modified

--- src/login.js
+++ src/login.js
@@ -41,9 +41,9 @@ function LoginPage() {
 
                         <form className="flex flex-col gap-4">
                     
-                            <input className="ring-1 ring-gray-300 focus:ring-2 focus:ring-[#9D9477] p-2 mt-8 rounded-xl border bg-[#f3f7fa]" type="text" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required/>
+                            <input className="p-2 mt-8 rounded-xl border outline-none focus:border-2 focus:border-[#5E6055] bg-[#f3f7fa]" type="text" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required/>
                             <div className="relative">
-                                <input className="ring-1 ring-gray-300 focus:ring-2 focus:ring-[#9D9477] p-2 rounded-xl border w-full bg-[#f3f7fa] " type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required/>
+                                <input className="p-2 rounded-xl border outline-none focus:border-2 focus:border-[#5E6055] w-full bg-[#f3f7fa] " type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required/>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="text-[#1E2124]" class="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
                                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
@@ -60,7 +60,7 @@ function LoginPage() {
                         <p className="mt-5 text-[#f3f7fa] text-xs underline cursor-pointer border-b border-gray-400 py-4">Forgot your password?</p>
                         <div className="mt-3 text-xs flex justify-between items-center">
                            <p className="text-[#f3f7fa] cursor-pointer">Don't Have An Account?</p>
-                           <Link className="text-[#c7bfa7] underline font-semibold hover:scale-105 duration-200" to="/signup">Sign Up</Link>
+                           <Link className="text-[#c7bfa7] hover:underline font-semibold hover:scale-105 duration-200" to="/signup">Sign Up</Link>
                         </div>
                    
                 </div>
--- src/signup.js
+++ src/signup.js
@@ -1,8 +1,8 @@
 function Signup() {
   return (
     <body>
-      <div className="h-screen flex items-center justify-center bg-[#748A8C]">
-        <div className="mr-56 max-w-sm mx-auto p-8 bg-[#012326] rounded-xl shadow-md space-y-2 flex-auto">
+      <div className="h-screen flex items-center justify-center bg-[#748A8C] bg-cover bg-no-repeat" style={{"background-image": `url('https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')`,}}>
+        <div className="mr-56 max-w-sm mx-auto p-8 bg-[#012326] bg-opacity-60 backdrop-blur-md rounded-xl shadow-md space-y-2 flex-auto">
           <div className="text-center space-y-2 sm:text-left">
             <h1 className="mb-4 text-base text-center font-semibold text-[#F2F2F2]">
               Register your free account and get immidiate access to online
@@ -11,42 +11,42 @@ function Signup() {
             <div className="border-t border-[#F2F2F2] my-4"></div>
           </div>
           <div>
-            <p className="text-body py-2 text-[#748A8C] font-medium">
+            <p className="text-body py-2 text-[#F2F2F2] font-medium">
               Username
             </p>
             <input
               type="text"
               placeholder="Username"
-              class="w-full rounded-sm border border-[#F2F2F2] py-1 px-2"
+              class="border outline-none focus:border-2 focus:border-green-500 w-full rounded-sm py-1 px-2"
             ></input>
-            <p className="text-body py-2 text-[#748A8C] font-medium ">
+            <p className="text-body py-2 text-[#F2F2F2] font-medium ">
               Email Address
             </p>
             <input
               type="text"
               placeholder="Email Address"
-              class="w-full rounded-sm border border-[#F2F2F2] py-1 px-2"
+              class="border outline-none focus:border-2 focus:border-green-500 w-full rounded-sm py-1 px-2"
             ></input>
-            <p className="text-body py-2 text-[#748A8C] font-medium ">
+            <p className="text-body py-2 text-[#F2F2F2] font-medium ">
               Password
             </p>
             <input
               type="text"
               placeholder="Password"
-              class="w-full rounded-sm  border border-[#F2F2F2] py-1 px-2"
+              class="w-full rounded-sm border outline-none focus:border-2 focus:border-green-500 py-1 px-2"
             ></input>
-            <p className="text-body py-2 text-[#748A8C] font-medium ">
+            <p className="text-body py-2 text-[#F2F2F2] font-medium ">
               Confirm Password
             </p>
             <input
               type="text"
               placeholder="Confirm Password"
-              class="w-full rounded-sm border border-[#F2F2F2] py-1 px-2"
+              class="w-full rounded-sm border outline-none focus:border-2 focus:border-green-500 py-1 px-2"
             ></input>
             <div class="mt-5">
               <button
-                className="w-full py-3 text-center border rounded-full text-sm font-semibold  hover:text-[#F2F2F2] hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2
-         border-[#5ba66e] text-[#49ad62] hover:bg-green-500 focus:ring-green-600 dark:text-green-400"
+                className="w-full py-3 text-center border rounded-full text-sm font-semibold hover:text-[#F2F2F2] hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2
+         border-[#5ba66e] text-[#49ad62] hover:bg-green-500 focus:ring-green-600 dark:text-green-400 hover:scale-105 duration-200"
               >
                 Sign Up
               </button>
