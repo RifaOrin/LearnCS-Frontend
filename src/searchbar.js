750911e658f18dd2f55f8cfcab579a52f761107f
Maruf <mrfshahriar@gmail.com>
2023-07-21 21:57:32 +0600

    Search option activate

--- src/searchbar.js
+++ src/searchbar.js
@@ -1,21 +1,45 @@
-function Searchbar(){
-    return(
-        <div class='max-w-md mx-auto px-3 py-3'>
-            <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-[#00242c8e] overflow-hidden">
-                <div class="grid place-items-center h-full w-12 text-gray-300">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                    </svg>
-                </div>
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-                <input class="peer h-full w-full outline-none text-sm bg-[#00242c8e]  pl-3 text-white"
-                 type="text"
-                 id="search"
-                  placeholder="Search something.." /> 
+function Searchbar({ history }) {
+    const [searchValue, setSearchValue] = useState("");
+
+    const handleChange = (e) => {
+        setSearchValue(e.target.value);
+    };
+
+    return (
+        <div className="max-w-md mx-auto px-3 py-3">
+            <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-[#00242c8e] overflow-hidden">
+                <Link to={`/searchResult/${searchValue}`}>
+                    <div className="grid place-items-center h-full w-12 text-gray-300">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                            />
+                        </svg>
+                    </div>
+                </Link>
+                <input
+                    className="peer h-full w-full outline-none text-sm bg-[#00242c8e] pl-3 text-white"
+                    type="text"
+                    id="search"
+                    placeholder="Search something.."
+                    value={searchValue}
+                    onChange={handleChange}
+                />
             </div>
         </div>
-            
-
     );
 }
+
 export default Searchbar;
