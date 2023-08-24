91178bdeb3334358da1ce655ea46190f7f05fa58
RifaOrin <rifaorin1234@gmail.com>
2023-08-24 12:49:16 +0600

    Landpage navbar responsive work done for xl devices

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -16,14 +16,14 @@ function LandingPage() {
     };
     return (
         <body>
-            <div className="container h-21 w-full bg-[#00242cc9] pl-16 pr-10 py-0.25 absolute z-20">
-                <nav className="flex items-center justify-between">
+            <div className=" h-21  bg-[#00242cc9] pl-16 pr-10 py-0.25 absolute z-20">
+                <nav className=" flex items-center justify-between">
                     <img src={Logo} className="w-1/6 h-21 cursor-pointer"></img>
                     <ul className="flex-1 text-right">
                         <li className="list-none inline-block px-5">
                             <Link
                                 to="/"
-                                className="no-underline text-[#F2F2F2] text-lg font-semibold px-3 py-3  hover:border-2 hover:scale-105 duration-250 cursor-pointer"
+                                className="no-underline text-[#F2F2F2] text-lg font-semibold px-3 py-3 hover:border-2 hover:scale-105 duration-250 cursor-pointer"
                             >
                                 Home
                             </Link>
@@ -66,8 +66,9 @@ function LandingPage() {
                         </li>
                         
                     </ul>
+                    <Searchbar/>
 
-                    <Searchbar />
+                    
                 </nav>
             </div>
 
@@ -85,13 +86,13 @@ function LandingPage() {
                 <div className="overlay absolute top-0 left-0 w-full h-full bg-overlay blend-screen"></div>
 
                 <div className="text relative">
-                    <h2 className="text-4xl  my-5 font-extrabold uppercase 2xl:text-5xl">
+                    <h2 className="text-4xl xl:text-5xl  my-5 font-extrabold uppercase 2xl:text-5xl">
                         Unlock the world of CS
                     </h2>
-                    <h3 className="text-3xl my-5 font-bold max-w-2xl 2xl:text-4xl">
+                    <h3 className="text-3xl xl:text-4xl my-5 font-bold max-w-2xl 2xl:text-4xl">
                         Knowledge with LearnCS
                     </h3>
-                    <p className="max-w-2xl my-5 text-base font-normal leading-7 text-justify">
+                    <p className="max-w-2xl my-5 text-base xl:text-lg font-normal leading-7 text-justify">
                         Discover a world of endless possibilities in the realm
                         of Computer Science, as LearnCS empowers you to master
                         new skills, broaden your horizons, and unleash your
