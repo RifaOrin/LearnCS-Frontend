6226f4f5c7f602785066ba200a302b9b7babec74
RifaOrin <rifaorin1234@gmail.com>
2023-08-28 01:05:22 +0600

    Chat page design added

--- src/chat.js
+++ src/chat.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from "react";
 import { db } from './firebase';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
-import './chatstyle.css'; // Import your styles here
+import { IonIcon } from "@ionic/react";
+import Navbar from "./navbar";
+import { send } from "ionicons/icons";
+
 
 function Chat() {
     const [messages, setMessages] = useState([]);
@@ -27,26 +30,27 @@ function Chat() {
     }
 
     return (
-        <div className="chat-container flex flex-col h-screen bg-gray-100">
-        <div className="chat-messages overflow-y-auto flex-grow px-4">
-            {messages.map(({ id, text, username }) => (
-                 
-                <div
-                    key={id}
-                    className={`message ${
-                        username === myUsername ? 'my-message' : 'other-message'
-                    }`}
-                >
-                   
-                   {username !== myUsername && <span className="message-sender">{username}</span>}
-                   {username === myUsername && <span className="message-sender">{username}</span>}
-                    <p className="px-2 py-1">{text}</p>
-                </div>
-            ))}
+        <body className="min-h-screen bg-gradient-to-b from-gray-400 to-gray-950">
+            <Navbar />
+        <div className="m-3 h-[83vh] max-w-lg mx-auto rounded-lg p-5 shadow-md flex flex-col bg-gradient-to-b from-gray-800 to-gray-950">
+        <div className=" overflow-y-auto scrollbar-thin scrollbar-thumb-[#433491] scrollbar-track-gray-400 grow px-4">
+                {messages.map(({ id, text, username }) => (
+                        <div key={id} className="flex flex-col">
+                            <span className={`text-sm text-[#888] mb-2 ${username === myUsername ? 'ml-auto' : 'mr-auto'} `}>
+                                {username}
+                            </span>
+                            
+                            <div className={`max-w-xs p-2.5 rounded-full mb-2.5 ${username === myUsername ? 'bg-[#433491] ml-auto text-white rounded-full font-medium' : 'bg-white text-[#333] font-medium mr-auto'} `}>
+                                <p className="px-2 py-1">{text}</p>
+                            </div>
+                            
+                        </div>
+                    ))}
         </div>
+
     
         <form
-            className="chat-input flex items-center border-t-2 border-gray-200 py-2 px-4"
+            className="flex grow items-center border-t-2 border-gray-600 py-2 px-4"
             onSubmit={handleMessage}
         >
             <input
@@ -54,16 +58,20 @@ function Chat() {
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
                 placeholder="Type your message..."
-                className="flex-grow border border-gray-300 rounded-full py-1 px-3 focus:outline-none"
+                className="mt-2 flex-grow border bg-transparent rounded-full text-gray-200 py-3 px-4 font-medium focus:outline-none"
             />
             <button
                 type="submit"
-                className="ml-3 bg-blue-500 hover:bg-blue-600 text-white rounded-full px-4 py-1 focus:outline-none"
+                className="ml-3 mt-2"
             >
-                Send
+                <IonIcon
+                    icon={send}
+                    className="text-3xl text-[#433491] hover:text-[#2e2070]"
+                />
             </button>
         </form>
     </div>
+    </body>
     
     );
 }
